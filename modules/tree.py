19a4fcf130e3ebc5b6a29217509730dbdac605d5
André Arruda <andre.luizarruda@ufrpe.br>
2017-08-03 17:00:37 -0300

    LeftRotation concluído

--- modules/tree.py
+++ modules/tree.py
@@ -30,7 +30,6 @@ class BinaryTree:
 	def RBRemove(self):
 		# método de remover um elemento
 
-	#André modificando...
 	def LeftRotation(self, noh):
 		# método de rotacionar pra esquerda
 		#filho direito de noh ocupará pos de noh
@@ -38,7 +37,25 @@ class BinaryTree:
 		# filho esquerdo de y será filho direito de noh
 		noh.SetRight(y.GetLeft())
 		# se o filho esquerdo de y nao for nulo, noh será pai dele
-		if y.GetLeft() != self.nulo
+		if y.GetLeft() != self.nulo:
+			y.GetLeft().SetParent(noh)
+		#pai de noh agora se torna pai de y (também)
+		y.SetParent(noh.GetParent())
+		# se pai de noh for nulo, quer dizer que noh era a raiz da árvore
+		#portanto, y passa a ser a raiz da árvore
+		if noh.getParent() == self.nulo:
+			self.SetRoot(y)
+		# se pai de noh não for nulo, quer dizer que noh é filho direito ou esquerdo (óbvio)
+		#precisamos disso para colocar y como novo filho do pai de noh (esquerdo ou direito)
+		elif noh == noh.GetParent().GetLeft():
+			# ao inves de noh, também poderiamos usar y no comando abaixo
+			noh.GetParent().SetLeft(y)
+		#se noh for filho direito, então y será filho direito do pai de nó
+		else:
+			noh.GetParent().SetRight(y)
+		#só falta definir o pai de noh como y e o filho esquerdo de y como nó
+		y.SetLeft(noh)
+		noh.SetParent(y)
 		
 	
 	#André modificando...
