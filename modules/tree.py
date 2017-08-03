2362ed429da43589406b5c022c2696d3d529c296
André Arruda <andre.luizarruda@ufrpe.br>
2017-08-03 16:34:13 -0300

    LeftRotation
    Implementando LeftRotation

--- modules/tree.py
+++ modules/tree.py
@@ -7,6 +7,8 @@ class BinaryTree:
 	def __init__(self):
 		# construtor da árvore
 		self.__root = self.nulo = Node(None, None)
+		# cor de nulo deve ser preta (Todo nó criado tem cor vermelha - por padrão)
+		self.nulo.SetIsRed(False)
 
 	#retorna a raiz da arvore
 	def GetRoot(self):
@@ -29,8 +31,15 @@ class BinaryTree:
 		# método de remover um elemento
 
 	#André modificando...
-	def LeftRotation(self):
+	def LeftRotation(self, noh):
 		# método de rotacionar pra esquerda
+		#filho direito de noh ocupará pos de noh
+		y = noh.GetRight()
+		# filho esquerdo de y será filho direito de noh
+		noh.SetRight(y.GetLeft())
+		# se o filho esquerdo de y nao for nulo, noh será pai dele
+		if y.GetLeft() != self.nulo
+		
 	
 	#André modificando...
 	def RightRotation(self):
