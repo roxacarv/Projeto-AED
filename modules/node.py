fb03ce46b85425dcf81e290dbfe6abf7a2439100
roxac <xavier.rc791@gmail.com>
2017-08-03 12:34:22 -0300

    Primeiro commit - base da árvore rubro-negra

---
+++ modules/node.py
@@ -0,0 +1,38 @@
+# Árvore Rubro-negra - AED
+# Projeto AED - BSI 2017.1 - UFRPE
+###################################
+
+class Node:
+
+	def __init__(self):
+		# construtor do nó
+
+	def GetParent(self):
+		# retorna o pai do nó
+
+	def GetLeft(self):
+		# retorna o filho esquerdo do nó
+
+	def GetRight(self):
+		# retorna o filho direito do nó
+
+	def GetData(self):
+		# retorna os dados do nó
+
+	def GetKey(self):
+		# retorna a chave do nó // usada para identificar a posição na árvore
+
+	def GetIsRed(self):
+		# retorna a cor do nó // preto = False e vermelho = True
+
+	def SetParent(self):
+		# seta o pai do nó
+
+	def SetRight(self):
+		# seta o filho direito do nó
+
+	def SetLeft(self):
+		# seta o filho esquerdo do nó
+
+	def SetIsRed(self):
+		# seta a cor do nó
---
+++ modules/tree.py
@@ -0,0 +1,41 @@
+# Árvore Rubro-negra - AED
+# Projeto AED - BSI 2017.1 - UFRPE
+###################################
+
+class BinaryTree:
+
+	def __init__(self):
+		# construtor da árvore
+
+	def RBFixup(self):
+		# fix das cores pra o balanceamento
+
+	def RBRemoveFixUp(self):
+		# fix das cores durante a remoção de um elemento
+
+	def RBInsert(self):
+		# método de inserir um elemento
+
+	def RBRemove(self):
+		# método de remover um elemento
+
+	def LeftRotation(self):
+		# método de rotacionar pra esquerda
+
+	def RightRotation(self):
+		# método de rotacionar pra direita
+
+	def TreeWalk(self):
+		# caminhada
+
+	def Maximum(self):
+		# retorna o número máximo
+
+	def Minimum(self):
+		# retorna o número mínimo
+
+	def Successor(self):
+		# retorna o número sucessor à entrada
+
+	def Predecessor(self)::
+		# retorna o número antecessor à entrada
