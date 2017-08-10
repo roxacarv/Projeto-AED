aa59803dbeddabc3a31aab0eac73736575fa14f8
roxac <xavier.rc791@gmail.com>
2017-08-10 00:16:58 -0300

    Adicionado um except pra não rolar erro se valores digitados forem errados

--- modules/interface.py
+++ modules/interface.py
@@ -24,88 +24,110 @@ def IniciaInterface():
 	elif escolha == "5":
 		RealizarCompra()
 		IniciaInterface()
+	else:
+		print("um valor foi digitado incorretamente")
 
 def RealizarCompra():
-    #É necessário receber também a hora em que a compra foi efetuada (verificar horario establecimento)
-    print("Digite o valor da compra, o número do cartão, o estabelecimento e a hora da compra separados por espaços:\n", end="")
-    valor, numero, estabelecimento, horaCompra = input().split(" ", 3)
-    #ATENÇÃO: cartao é um objeto do tipo no, não do tipo cartao
-    cartao = VerificarCartao(int(numero))
-    #Verifica se o cartao existe
-    if cartao == None:
-        print("Esse cartão não existe, a compra não pode ser realizada")
-        IniciaInterface()
-    else:
-        #Se existir, verifica se o cartao tem limite suficiente para a compra
-        if VerificarLimiteByObj(cartao, float(valor)):
-            print("Há limite para realizar a compra")
-            #Agora deve-se verificar se o estabelecimento esta cadastrado
-            resultBuscaEstabelecimento = VerificarEstabelecimento(estabelecimento)
-            #Se estabelecimento estiver cadastrado
-            if resultBuscaEstabelecimento != None:
-                print("Estabelecimento está cadastrado!")
-                #Agora deve-se verificar se a compra foi efetuada em horário comercial
-                horarioOk = VerificaHorarioComercial(horaCompra, resultBuscaEstabelecimento.GetData())
-                #Se compra for efetuada em horario comercial
-                if horarioOk:
-                    cartao.GetData().desconto(float(valor))
-                    cartao.GetData().AddMontante(float(valor))
-                    resultBuscaEstabelecimento.GetData().AddMontante(((float(valor)*0.02)+float(valor)))
-                    resultBuscaEstabelecimento.GetData().AddValor((float(valor)*0.02))
-                    print("Limite atual do cartao: R$ %.2f" % (cartao.GetData().getLimiteAtual()))
-                    print("*"*20)
-                    print("Compra autorizada!")
-                    print("*"*20)
-                    IniciaInterface()
-                else:
-                	print("Estabelecimento fora do horário de funcionamento.")
-                	IniciaInterface()
-            #Se estabelecimento não estiver cadastrado
-            else:
-                print("Estabelecimento não está cadastrado!")
-                IniciaInterface()
-        #Caso cartao não tenha limite para efetuar a compra
-        else:
-            print("Não há limite suficiente para realizar a compra")
-            IniciaInterface()
+	try:
+	    #É necessário receber também a hora em que a compra foi efetuada (verificar horario establecimento)
+	    print("Digite o valor da compra, o número do cartão, o estabelecimento e a hora da compra separados por espaços:\n", end="")
+	    valor, numero, estabelecimento, horaCompra = input().split(" ", 3)
+	    #ATENÇÃO: cartao é um objeto do tipo no, não do tipo cartao
+	    cartao = VerificarCartao(int(numero))
+	    #Verifica se o cartao existe
+	    if cartao == None:
+	        print("Esse cartão não existe, a compra não pode ser realizada")
+	        IniciaInterface()
+	    else:
+	        #Se existir, verifica se o cartao tem limite suficiente para a compra
+	        if VerificarLimiteByObj(cartao, float(valor)):
+	            print("Há limite para realizar a compra")
+	            #Agora deve-se verificar se o estabelecimento esta cadastrado
+	            resultBuscaEstabelecimento = VerificarEstabelecimento(estabelecimento)
+	            #Se estabelecimento estiver cadastrado
+	            if resultBuscaEstabelecimento != None:
+	                print("Estabelecimento está cadastrado!")
+	                #Agora deve-se verificar se a compra foi efetuada em horário comercial
+	                horarioOk = VerificaHorarioComercial(horaCompra, resultBuscaEstabelecimento.GetData())
+	                #Se compra for efetuada em horario comercial
+	                if horarioOk:
+	                    cartao.GetData().desconto(float(valor))
+	                    cartao.GetData().AddMontante(float(valor))
+	                    resultBuscaEstabelecimento.GetData().AddMontante(((float(valor)*0.02)+float(valor)))
+	                    resultBuscaEstabelecimento.GetData().AddValor((float(valor)*0.02))
+	                    print("Limite atual do cartao: R$ %.2f" % (cartao.GetData().getLimiteAtual()))
+	                    print("*"*20)
+	                    print("Compra autorizada!")
+	                    print("*"*20)
+	                    IniciaInterface()
+	                else:
+	                	print("Estabelecimento fora do horário de funcionamento.")
+	                	IniciaInterface()
+	            #Se estabelecimento não estiver cadastrado
+	            else:
+	                print("Estabelecimento não está cadastrado!")
+	                IniciaInterface()
+	        #Caso cartao não tenha limite para efetuar a compra
+	        else:
+	            print("Não há limite suficiente para realizar a compra")
+	            IniciaInterface()
+	except:
+		print("Você digitou um valor ou valores incorretos. Por favor, tente novamente.")
+		IniciaInterface()
 
 def BuscarCartao():
-	print("Digite o número do cartão a ser buscado: ", end="")
-	numero = int(input())
-	cartao = VerificarCartao(numero)
-	if cartao == None:
-		print("Esse cartão não existe no sistema")
-	else:
-		print("-"*30)
-		print("Nome do Titular: %s\nNúmero do Cartão: %s\nLimite Atual: %s" % (cartao.GetData().getTitular(), cartao.GetData().getNumero(), cartao.GetData().getLimiteAtual()))
-		print("-"*30)
+	try:
+		print("Digite o número do cartão a ser buscado: ", end="")
+		numero = int(input())
+		cartao = VerificarCartao(numero)
+		if cartao == None:
+			print("Esse cartão não existe no sistema")
+		else:
+			print("-"*30)
+			print("Nome do Titular: %s\nNúmero do Cartão: %s\nLimite Atual: %s" % (cartao.GetData().getTitular(), cartao.GetData().getNumero(), cartao.GetData().getLimiteAtual()))
+			print("-"*30)
+	except:
+		print("Você digitou um valor ou valores incorretos. Por favor, tente novamente.")
+		IniciaInterface()
 
 def BuscarEstabelecimento():
-	print("Digite o nome do estabelecimento a ser buscado: ", end="")
-	nome = input()
-	estabelecimento = VerificarEstabelecimento(nome)
-	if estabelecimento == None:
-		print("Esse estabelecimento não existe no sistema")
-	else:
-		print("-"*30)
-		print("Nome do Estabelecimento: %s\nHorário de Funcionamento: %s" % (estabelecimento.GetData().getNome(), estabelecimento.GetData().getHorario()))
-		print("-"*30)
+	try:
+		print("Digite o nome do estabelecimento a ser buscado: ", end="")
+		nome = input()
+		estabelecimento = VerificarEstabelecimento(nome)
+		if estabelecimento == None:
+			print("Esse estabelecimento não existe no sistema")
+		else:
+			print("-"*30)
+			print("Nome do Estabelecimento: %s\nHorário de Funcionamento: %s" % (estabelecimento.GetData().getNome(), estabelecimento.GetData().getHorario()))
+			print("-"*30)
+	except:
+		print("Você digitou um valor ou valores incorretos. Por favor, tente novamente.")
+		IniciaInterface()
 
 def CadastraCartao():
-	print("Digite o nome do titular, o numero do cartão e qual será o limite total separado por espaço;")
-	titular, numero, limite = input().split(" ", 2)
-	if not CadastrarCartao(int(numero), titular, int(limite)):
-		print("O cartão já está cadastrado no sistema")
-	else:
-		print("O cartão foi cadastrado com sucesso!")
+	try:
+		print("Digite o nome do titular, o numero do cartão e qual será o limite total separado por espaço;")
+		titular, numero, limite = input().split(" ", 2)
+		if not CadastrarCartao(int(numero), titular, int(limite)):
+			print("O cartão já está cadastrado no sistema")
+		else:
+			print("O cartão foi cadastrado com sucesso!")
+	except:
+		print("Você digitou um valor ou valores incorretos. Por favor, tente novamente.")
+		IniciaInterface()
 
 def CadastraEstabelecimento():
-	print("Digite o nome do estabelecimento e o horário de funcionamento separado por espaço;")
-	nome, horario = input().split(" ", 2)
-	if not CadastrarEstabelecimento(nome, horario):
-		print("O estabelecimento já se encontra cadastrado no sistema")
-	else:
-		print("O estabelecimento foi cadastrado com sucesso!")
+	try:
+		print("Digite o nome do estabelecimento e o horário de funcionamento separado por espaço;")
+		nome, horario = input().split(" ", 2)
+		if not CadastrarEstabelecimento(nome, horario):
+			print("O estabelecimento já se encontra cadastrado no sistema")
+		else:
+			print("O estabelecimento foi cadastrado com sucesso!")
+	except:
+		print("Você digitou um valor ou valores incorretos. Por favor, tente novamente.")
+		IniciaInterface()
 
 
 IniciaInterface()
